1f9afefcacc8ea9e9f38be2a6cfa363e8b0fa511
Adrian Kokai <adriankokai@gmail.com>
2021-03-08 21:53:33 +0200

    setup the app

---
+++ src/store/utility.js
@@ -0,0 +1,9 @@
+export const updateObject = (state, updatedObject) => {
+    return ({
+        state,
+        ...updatedObject
+    })
+}
+
+export const BACKEND_URL = 'http://127.0.0.1:8000/'
+//export const BACKEND_URL = 'https://kazmarketbackend.herokuapp.com/'
