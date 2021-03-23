f50db7ac16efb4bb8846228c25dffda7700ed800
Adrian Kokai <adriankokai@gmail.com>
2021-03-23 18:38:57 +0200

    add scrollable category buttons to home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import Navbar from '../components/Navbar/Navbar';
 import Categories from '../components/category/Categories';
-import Footer from '../components/footer/Footer.js'
+import Footer from '../components/footer/Footer.js';
+
 
 export class Home extends Component {
     render() {
         return (
             <div>
                 <Navbar />
+                <Categories key={1} buttons={true} />
                 <br /><br />
                 <div style={styles}>
                     <h5>Browse By Category</h5>
-                    <Categories />
+                    <Categories key={2} />
                 </div>
                 <Footer />
             </div>
