0c4f421219275a6012a4837d5beb2ad8dad01fae
Adrian Kokai <adriankokai@gmail.com>
2021-03-24 08:57:56 +0200

    change BACKEND_URL

--- src/store/utility.js
+++ src/store/utility.js
@@ -5,6 +5,6 @@ export const updateObject = (state, updatedObject) => {
     })
 }
 
-export const BACKEND_URL = 'http://127.0.0.1:8000/'
-//export const BACKEND_URL = 'https://kazmarketbackend.herokuapp.com/'
+//export const BACKEND_URL = 'http://127.0.0.1:8000/'
+export const BACKEND_URL = 'https://kazmarketbackend.herokuapp.com/'
 //export const BACKEND_URL_NO_LEADING_SLASH = 'http://127.0.0.1:8000'
